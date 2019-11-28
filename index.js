025ac62ec4935d5faad4e7bde3b9b47d4d7b2f34
Philippe Villette <iphone6060@gmail.com>
2019-11-28 17:52:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: process.env.PORT || 8080 }, () => {
+  console.log("Server started");
+});
+
+wss.on("connection", connection => {
+  console.log("nouvelle connection");
+
+  connection.on("message", message => {
+    // Transmettre un message à tous les autres utilisateurs connectés (broadcast)
+    wss.clients.forEach(client => {
+      console.log("nouveau message", message);
+      if (client !== connection && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
