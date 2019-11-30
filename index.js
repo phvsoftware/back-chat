cd75d026e91e7409d50cfd08de4a8c2a4a30f23b
Philippe Villette <iphone6060@gmail.com>
2019-11-30 20:58:25 +0100

    ajout upload image dans cloudinary

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-const WebSocket = require("ws");
+require("dotenv").config();
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 8080 }, () => {
-  console.log("Server started");
+const cors = require("cors");
+const formidableMiddleware = require("express-formidable");
+const WebSocket = require("ws");
+const cloudinary = require("cloudinary").v2;
+const express = require("express");
+const http = require("http");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const app = express();
+app.use(formidableMiddleware());
+app.use(cors());
+
+app.post("/upload", async (req, res) => {
+  console.log("route upload");
+  // upload de l'image
+  try {
+    cloudinary.uploader.upload(req.files.picture.path, async function(
+      error,
+      result
+    ) {
+      if (!error) {
+        console.log("upload ok");
+        const url = result.secure_url;
+        // message de retour
+        res.json({ picture: url });
+      } else {
+        console.log("upload échec");
+        res.status(400).json({ message: "An error occurred" });
+      }
+    });
+  } catch (e) {
+    console.log("catch", e);
+    res.status(400).json({ message: "An error occurred" });
+  }
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server: server });
 
 wss.on("connection", connection => {
   console.log("nouvelle connection");
@@ -16,4 +56,8 @@ wss.on("connection", connection => {
       }
     });
   });
+});
+
+server.listen(process.env.PORT || 8080, () => {
+  console.log("Server started on port ", process.env.PORT || 8080);
 });
